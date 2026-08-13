cd076f9101f11a1ce947fc2898bf5197b3e58b85
Rodrigo <ralonzo1337@gmail.com>
2026-08-13 19:55:24 -0300

    fix: update image import paths and hover effects

--- Rodrigo-portfolio/src/data/experience.ts
+++ Rodrigo-portfolio/src/data/experience.ts
@@ -1,4 +1,4 @@
-import InitialImage from "../assets/experience/initial.png";
+import InitialImage from "../assets/experience/Initial.png";
 import FreelanceImage from "../assets/experience/freelance.jpg";
 import wordpressImg from "../assets/experience/wordpress.jpg";
 import architectureImg from "../assets/experience/architecture.jpg";
